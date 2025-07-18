b7fe51dd394af9f6964fe06542260e84f987cc5a
Firasync <161107983+Firaoll77@users.noreply.github.com>
2025-07-18 23:46:55 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Welcome</title>
+       <title>Welcome</title>
 
        <h1>Welcome To Task Manager backend </h1>
     </body>
