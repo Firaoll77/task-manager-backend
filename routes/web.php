22767bd3defbb3752e070c315f778987467e7c07
Firasync <161107983+Firaoll77@users.noreply.github.com>
2025-07-20 23:46:57 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+use App\Http\Controllers\PostController;
+
+// Define a named route for showing a post
+Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
+
+// Define a named route for creating a post
+Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+
+// Define a named route for storing a post
+Route::post('/post', [PostController::class, 'store'])->name('post.store');
+
+
 ?>
